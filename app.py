e37c1dfbd82a0628a82a69fcb19648fa2592199e
Aditya Bhatt <meenakshi.bhtt@gmail.com>
2024-04-26 15:40:17 +0530

    Basic app with 4 ai personal implemented

--- app.py
+++ app.py
@@ -5,24 +5,31 @@ Objective            -
 1.Create a system to automate social media management.
 
 Employee 1 Planning -
-1.Julia Get's Information from Wikipedia + Internet
+1.Julia Get's Information from  Internet
+2.Jinny Your AI Writer
+3.Jane your AI Editor
+4.John is your Artist
 
 AI System By Aditya
 '''
 import os
 import streamlit as st
-from langchain_community.tools import WikipediaQueryRun
-from langchain_community.utilities import WikipediaAPIWrapper
 from langchain.adapters.openai import convert_openai_messages
 from langchain_community.chat_models import ChatOpenAI
 from tavily import TavilyClient
-client = TavilyClient(api_key="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")
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from openai import AzureOpenAI
+import requests
+import json
+
+
 # run tavily search
 OPENAI_API_KEY ="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"
 os.environ['OPENAI_API_KEY']="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"
 
-api_wrapper = WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=2000)
-tool = WikipediaQueryRun(api_wrapper=api_wrapper)
 
 def main():
         st.title("Social MediaGPT")
@@ -38,13 +45,12 @@ def main():
         
 
         if st.button("Start the Magic"):
-            st.write("Julia(Your AI Researcher is at it)...")
+            st.header("Julia(Your AI Researcher is at it)...")
             st.image("JULIA.png")
-            st.write("She is searching in wikipedia...")
             try:
-                wiki_result=tool.run({"query": f"{title}"})
-                st.write(wiki_result)
+                #AI Agent -1 Researcher
                 st.write("Julia is searching over the internet...")
+                client = TavilyClient(api_key="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")
                 content = client.search(title, search_depth="advanced")["results"]
                 # setup prompt
                 prompt = [{
@@ -60,7 +66,7 @@ def main():
                             f'Please use MLA format and markdown syntax.'
                 }]
 
-                st.write("Julia is still working.... what you think research is an easy task...😄")
+                st.write("Julia is still working.... what you thought research is an easy task...😄")
 
                 # run gpt-3.5
                 lc_messages = convert_openai_messages(prompt)
@@ -69,15 +75,75 @@ def main():
                 # print report
                 st.write("Here the report which Julia has made")
                 st.write(report)
-                context=[wiki_result,report]
+    
                 st.write("App in Development") 
 
-            except Exception as e:
-                  st.write("Julia is not working as expected")
-            pass
-            #AI Agent -1 Researcher
+                
 
-  
+                 #Agent -2 Simi Writer
+                st.header("Jinny your Writer is  Writing the text...")
+                st.image("Simi.PNG")
+                prompt = ChatPromptTemplate.from_template(
+                "Write a deatalied Social Media Post about {topic} in a {tone}and add relevant hashtags take points from text provided in input and add your knowledge as well"   
+                )
+                output_parser = StrOutputParser()
+                model = ChatOpenAI(model="gpt-3.5-turbo")
+                chain = (
+                    {"topic": RunnablePassthrough() , "tone": RunnablePassthrough()} 
+                    | prompt
+                    | model
+                    | output_parser
+                )
+                result=chain.invoke([report,tone_of_post])
+                st.write(result)
+                #Agent 3 Jane
+                st.header("Jane your Editor is  Editing the text...")
+                st.image("JANE.PNG")
+                prompt = ChatPromptTemplate.from_template(
+                "Act as editor make sure no grammer errors are in the {topic}.Output the same text with grammer modifications"   
+                )
+                chain = (
+                    {"topic": RunnablePassthrough()} 
+                    | prompt
+                    | model
+                    | output_parser
+                )
+                result=chain.invoke(result)
+                st.write(result)
+
+                #Agent 4 Artist
+                st.header("John your own artist is creating a image for you...")
+                st.image("John.PNG")
+                client = AzureOpenAI(
+                api_version="2024-02-01",
+                azure_endpoint="https://aiagents.openai.azure.com/",
+                api_key="198e8394cb0f47e69807d2bcd7a26c60",
+                )
+
+                result = client.images.generate(
+                    model="sass", # the name of your DALL-E 3 deployment
+                    prompt=title,
+                    n=1
+                ) 
+
+                # Send a GET request to the URL
+                image_url = json.loads(result.model_dump_json())['data'][0]['url']
+                response = requests.get(image_url)
+
+                # Check if the request was successful (status code 200)
+                if response.status_code == 200:
+                    # Open a file in binary write mode and write the content of the response
+                    with open("image.jpg", "wb") as file:
+                       z= file.write(response.content)
+                    print("Image downloaded successfully.")
+                else:
+                    print("Failed to download image.")
+                
+                st.image("image.jpg")
+
+            except Exception as e:
+                  st.write("Julia is not working as expected",e)
+            
 
 
 if __name__ == "__main__":
